fcbebaa3d9804ce1a9a32c91e764cb8a770f7ec1
Moji <hassan.erfani@tao.at>
2024-12-14 01:15:35 +0100

    Bonus: menu options implementation

--- hangman.py
+++ hangman.py
@@ -4,19 +4,16 @@ def main():
     words = ['hello', 'campus', 'mathematik', 'python', 'weihnachten', 'joanneum',
     'softwaredesign', 'mojiverse', 'designpattern', 'bitcoin']
 
-    word = random.choice(words)
-    startMessage = print(f"Welcome to hangman. Your word will be: \n")
+    scoreboard = {}
 
-    guessedWord, tries = play(word)
+    # Using end='' in the print to prevent a newline.
+    startMessage = print(f"Welcome to hangman. ", end='')
 
-    # Success Message with the results.
-    print(f"Congratulation. You guessed '{guessedWord}' correctly after {tries} tries")
+    # Display the menu for user to choose an option.
+    menu(words, scoreboard)
 
-
-def play(word):
-    # Counter for tried guess.
-    tries = 0
-
+# Handle the game on new game.
+def play(word, tries):
     # Create a list of dots with the length of the word.
     toBeGuessed = ['.' for _ in range(len(word))]
 
@@ -28,18 +25,64 @@ def play(word):
 
         tries += 1 # Woow, increment a variable like tries++ is not possible in python :O
 
-    guessedWord = ' '.join(toBeGuessed)
+    guessedWord = ''.join(toBeGuessed)
     # Print the final word after guessing is finished
     print(guessedWord)
 
     return guessedWord, tries
 
+# Return the updated guessing word after currentGuess.
 def checkCurrentGuess(word, toBeGuessed, currentGuess):
     for i in range(len(word)):
         if word[i] == currentGuess.lower():
             toBeGuessed[i] = currentGuess
 
     return toBeGuessed
+
+def showScoreboard(scoreboard):
+    print(scoreboard)
+
+def menu(words, scoreboard):
+    tries = 0
+    word = random.choice(words)
+
+    # Menu options
+    options = input(f"""What will u do?\n
+[n]ew game\n
+[s]how score\n
+[e]nd game\n
+> """)
+
+    match options.lower():
+        case 'n':
+            guessedWord, tries = play(word, tries)
+
+            # Success Message with the results.
+            print(f"Congratulation. You guessed '{guessedWord}' correctly after {tries} tries")
+
+            # Update scoreboard for the successfully guessed word.
+            scoreboard.update([(word, tries)])
+
+            # remove the guessedWord from the words list.
+            words.remove(guessedWord)
+
+            # Reinitialize the word and tries.
+            tries = 0
+            word = random.choice(words)
+
+            # Call menu function again.
+            menu(words, scoreboard)
+        case 's':
+            print("Your score result's")
+            showScoreboard(scoreboard)
+
+            # Call menu function again.
+            menu(words, scoreboard)
+        case 'e':
+            print("Bye Bye")
+            exit()
+        case _:
+            exit()
 
 
 # Initialize the main function.
